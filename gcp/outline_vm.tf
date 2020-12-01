1b68786274a7cd3634c9c6c3e9ac5139967b3683
Lunik <15705897+rjsgn@users.noreply.github.com>
2020-12-02 00:24:32 +0100

    Integrate Ansible provisioner

--- gcp/outline_vm.tf
+++ gcp/outline_vm.tf
@@ -25,4 +25,23 @@ resource "google_compute_instance" "outline_vm" {
     ssh-keys = "${var.user}:${file(var.public_key)}"
   }
 
+  provisioner "ansible" {
+    plays {
+      playbook {
+        file_path = "../ansible/main.yml"
+        tags = ["common", "node"]
+      }
+      enabled = true
+      hosts = [self.network_interface.0.access_config.0.nat_ip]
+      verbose = true
+    }
+    ansible_ssh_settings {
+      connect_timeout_seconds = 10
+      connection_attempts = 10
+      ssh_keyscan_timeout = 60
+      insecure_no_strict_host_key_checking = false
+      insecure_bastion_no_strict_host_key_checking = false
+    }
+  }
+
 }
