dd465f40424551a64dcfe428630630e17ce0ea53
Lunik <15705897+rjsgn@users.noreply.github.com>
2021-03-01 23:47:19 +0100

    Updated Readme

--- install-ansible.sh
+++ install-ansible.sh
@@ -2,7 +2,7 @@
 # Ansible Installer for Debian based Linux Distributions
 # Refer: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-on-ubuntu
 
-# Install curl
+# APT Update
 apt update
 
 # Install requirements
@@ -11,5 +11,5 @@ apt install software-properties-common
 # Add Ansible APT Repository
 apt-add-repository --yes --update ppa:ansible/ansible
 
-# Install Terraform
+# Install Ansible
 apt install ansible
